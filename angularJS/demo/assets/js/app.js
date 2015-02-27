50c0199645b63c224f37924f76e8bca3ddab8bf5
mglezh <mglezh@gmail.com>
2015-02-27 19:22:24 +0100

    Angular y HTML5

---
+++ angularJS/demo/assets/js/app.js
@@ -0,0 +1,13 @@
+var todoApp = angular.module('todoApp', []);
+
+todoApp.controller('TodoController', ['$scope',function($scope){
+	var todos = [];
+	todos.push({done:false, task: 'Aprender AngularJS'});
+	todos.push({done:false, task: 'Aprender Ionic'});
+	todos.push({done:true,  task: 'Aprender JS'});
+	$scope.todos = todos;
+
+	$scope.addTodo = function(){
+		todos.push({done:false, task: $scope.inputText});
+	};
+}]);
